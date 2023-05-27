b4a5c3340732ef1ade27e95d85d0f21b363d14cc
wanyh <wanyuhao@boe.com.cn>
2023-05-27 08:04:51 +0800

    20230527

--- Practice/script.js
+++ Practice/script.js
@@ -1,4 +1,20 @@
+/* Data Types */
 let firstName = "Jonas";
 console.log(firstName);
 
 let PI = 3.1415
+
+console.log(typeof firstName);
+
+console.log(typeof PI);
+
+let year;
+console.log(typeof year);
+
+
+/* let const var */
+let age = 30;
+age = 31
+
+const birthYear = 1993;
+// birthYear = 1991;
