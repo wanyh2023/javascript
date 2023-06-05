46c84d6385ec44b090d6ea8e2175d957353d39c6
wanyh2023 <wanyh2023@gmail.com>
2023-06-05 22:03:21 +0800

    20230605

--- 05-Guess-My-Number/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "True!";
-
-document.querySelector(".guess").value = 23;
-console.log(document.querySelector(".guess").value);
